3fa2dbc5b1d44bcf9f08fba52cdb4f9bfdbe6094
source-architect <source.architect@gmail.com>
2021-02-05 22:26:30 +0330

    working on rendering menu items with new data structure

--- components/CardsMenu/CardsMenu.js
+++ components/CardsMenu/CardsMenu.js
@@ -1,47 +1,68 @@
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
+import useFormatData from "./hooks/useFormatData";
 
 import ItemCard from "./ItemCard/ItemCard";
 
 export default function CardsMenu(props) {
   const { menuData } = props;
 
+  const { categories, popularItems } = useFormatData(menuData);
   const [activeCategory, setActiveCategory] = useState("popular");
 
   const activeCategoryHandler = (categoryTitle) => {
+    setActiveCategory(null);
     setActiveCategory(categoryTitle);
   };
 
-  if (menuData)
+  if (menuData && popularItems)
     return (
       <div className="cards-menu h-100 d-flex flex-column">
         <div className="cards-categories">
-          {Object.keys(menuData).map((categoryTitle) => {
+          {categories.map((category) => {
             return (
               <div
                 key={uuid()}
                 className={`category${
-                  activeCategory === categoryTitle ? " active" : ""
+                  activeCategory === category ? " active" : ""
                 }`}
-                onClick={() => activeCategoryHandler(categoryTitle)}
+                onClick={() => activeCategoryHandler(category)}
               >
-                {categoryTitle}
+                {category}
               </div>
             );
           })}
         </div>
         <div className="cards-container">
           <div className="flex-fill d-inline-block">
-            {menuData[activeCategory].map((item) => {
-              return (
-                <ItemCard
-                  key={uuid}
-                  title={item.title}
-                  description={item.description}
-                  image={item.image}
-                />
-              );
-            })}
+            {activeCategory === "popular"
+              ? popularItems.map((item) => {
+                  return (
+                    <ItemCard
+                      key={uuid}
+                      title={item.title}
+                      description={item.description}
+                      image={item.image}
+                    />
+                  );
+                })
+              : null}
+
+            {activeCategory !== null
+              ? menuData.map((item) => {
+                  if (item.category === activeCategory) {
+                    return (
+                      <ItemCard
+                        key={uuid}
+                        title={item.title}
+                        description={item.description}
+                        image={item.image}
+                      />
+                    );
+                  }
+                  return null;
+                })
+              : null}
           </div>
         </div>
       </div>
--- hooks/useMenuData.js
+++ hooks/useMenuData.js
@@ -1,399 +1,215 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const fakeData = {
-  burger: {
-    popular: [
-      {
-        title: `hamburger`,
-        description: `First there was the burger. Then the King came. The Hamburger from BURGER KING® is “the real thing”`,
-        image: "/design-utils/temp/hamburger.png",
-      },
-      {
-        title: `double steakhouse`,
-        description: `The Double Steakhouse is the only burger for the real gourmets. With no fewer than two pieces of 100% Flame Grilled Beef.`,
-        image: "/design-utils/temp/double-steakhouse.png",
-      },
-      {
-        title: `cheese lover chicken`,
-        description: `Is the cheese and chicken combination your favorite? Go for the Cheese Lover Chicken. A freshly toasted and delicious choice.`,
-        image: "/design-utils/temp/cheese-lover-chicken.png",
-      },
-    ],
-    beaf: [
-      {
-        title: `double steakhouse`,
-        description: `The Double Steakhouse is the only burger for the real gourmets. With no fewer than two pieces of 100% Flame Grilled Beef.`,
-        image: "/design-utils/temp/double-steakhouse.png",
-      },
-      {
-        title: `double bacon`,
-        description: `Bacon, cheese, ketchup and mayonnaise on a flame grilled beef burger. With 2x beef it is also twice as delicious!`,
-        image: "/design-utils/temp/double-bacon-burger.png",
-      },
-      {
-        title: `plant-base whopper`,
-        description: `The WHOPPER® for citizens who love plants more. The plant-based WHOPPER® contains a vegetable hamburger and vegetable mayonnaise.`,
-        image: "/design-utils/temp/plant-based-whopper.png",
-      },
-      {
-        title: `hamburger`,
-        description: `First there was the burger. Then the King came. The Hamburger from BURGER KING® is “the real thing”`,
-        image: "/design-utils/temp/hamburger.png",
-      },
-    ],
-    chicken: [
-      {
-        title: `cheese lover chicken`,
-        description: `Is the cheese and chicken combination your favorite? Go for the Cheese Lover Chicken. A freshly toasted and delicious choice.`,
-        image: "/design-utils/temp/cheese-lover-chicken.png",
-      },
-      {
-        title: `long chicken`,
-        description: `Long ago you could enjoy Chicken Burger for a short time. Until we came up with the Long Chicken; a sesame bun with a spicy chicken breast fillet.`,
-        image: "/design-utils/temp/long-chicken.png",
-      },
-      {
-        title: `crunchy chicken deluxe`,
-        description: `We have chicken and we have CHICKEN! The Crunchy Chicken Deluxe certainly belongs to the latter category.`,
-        image: "/design-utils/temp/crunchy-chicken-deluxe.png",
-      },
-      {
-        title: `crispy chicken`,
-        description: `Crunchy on the outside, soft on the inside. The Crispy Chicken has everything you would expect from a chicken burger.`,
-        image: "/design-utils/temp/crispy-chicken.png",
-      },
-    ],
-  },
-  pizza: {
-    popular: [
-      {
-        title: `Hot Chicken`,
-        description: `The Hot Chicken pizza is for people who love chicken and spicy food. This delicious pizza is a real favorite of everyone.`,
-        image: "/design-utils/temp/hot-chicken.png",
-      },
-      {
-        title: `Pepperoni Perfection`,
-        description: `The Pepperoni Perfection is a delicious variation on our successful Double Pepperoni pizza where everything revolves around super tasty pepperoni.`,
-        image: "/design-utils/temp/pepperoni-perfection.png",
-      },
-      {
-        title: `New York`,
-        description: `The New York pizza is the perfect lunch or evening meal. The New York pizza is topped with red tomato sauce, ground beef and turkey ham.`,
-        image: "/design-utils/temp/new-york.png",
-      },
-    ],
-    other: [
-      {
-        title: `BBQ mixed grill`,
-        description: `This barbecue mixed grill pizza has everything you want! Divine BBQ sauce, fresh mozzarella, onion and bell pepper.`,
-        image: "/design-utils/temp/bbq-mixed-grill.png",
-      },
-      {
-        title: `Vegan Shawarma`,
-        description: `The Vegan Shawarma pizza is one especially for you as a vegetarian or vegan. There are no animal products in the pizza.`,
-        image: "/design-utils/temp/vegan-shawarma.png",
-      },
-      {
-        title: `Hot Chicken`,
-        description: `The Hot Chicken pizza is for people who love chicken and spicy food. This delicious pizza is a real favorite of everyone.`,
-        image: "/design-utils/temp/hot-chicken.png",
-      },
-      {
-        title: `hot thai chicken`,
-        description: `Anyone who loves Asian food has ordered Thai food. And those who love Thai, lucky enough to order the Hot Thai Chicken Pizza.`,
-        image: "/design-utils/temp/hot-thai-chicken.png",
-      },
-      {
-        title: `Brooklyn Style`,
-        description: `Real American pizza: the Brooklyn Style pizza is a real American classic. The pizza is one of our toppers for a reason.`,
-        image: "/design-utils/temp/brooklyn-style.png",
-      },
-      {
-        title: `Pepperoni Perfection`,
-        description: `The Pepperoni Perfection is a delicious variation on our successful Double Pepperoni pizza where everything revolves around super tasty pepperoni.`,
-        image: "/design-utils/temp/pepperoni-perfection.png",
-      },
-      {
-        title: `New York`,
-        description: `The New York pizza is the perfect lunch or evening meal. The New York pizza is topped with red tomato sauce, ground beef and turkey ham.`,
-        image: "/design-utils/temp/new-york.png",
-      },
-    ],
-  },
-  drink: {
-    popular: [
-      {
-        title: `Caffee Americano`,
-        description: `Espresso shots topped with hot water create a light layer of crema culminating in this wonderfully rich cup with depth and nuance.`,
-        image: "/design-utils/temp/paper-cup.png",
-      },
-      {
-        title: `Lemonade`,
-        description: `Awaken your taste buds with the zing of refreshing lemonade—this tangy, fresh sip puts a little zip in your step.`,
-        image: "/design-utils/temp/lemonade.png",
-      },
-      {
-        title: `Hot Chocolate`,
-        description: `Steamed milk and mocha sauce topped with sweetened whipped cream and a chocolate-flavoured drizzle. A timeless classic made to sweeten your spirits.`,
-        image: "/design-utils/temp/hot-chocolate.png",
-      },
-    ],
-    hot: [
-      {
-        title: `Caffee Americano`,
-        description: `Espresso shots topped with hot water create a light layer of crema culminating in this wonderfully rich cup with depth and nuance.`,
-        image: "/design-utils/temp/paper-cup.png",
-      },
-      {
-        title: `Espresso`,
-        description: `Our smooth signature Espresso Roast with rich flavour and caramelly sweetness is at the very heart of everything we do.`,
-        image: "/design-utils/temp/paper-cup.png",
-      },
-      {
-        title: `Hot Chocolate`,
-        description: `Steamed milk and mocha sauce topped with sweetened whipped cream and a chocolate-flavoured drizzle. A timeless classic made to sweeten your spirits.`,
-        image: "/design-utils/temp/hot-chocolate.png",
-      },
-      {
-        title: `Green Tea`,
-        description: `Pure green tea is steamed with milk. A perfect hot companion day or night.`,
-        image: "/design-utils/temp/green-tea.png",
-      },
-    ],
-    cold: [
-      {
-        title: `Iced Caffè Americano`,
-        description: `Espresso shots topped with cold water produce a light layer of crema, then served over ice. The result: a wonderfully rich cup with depth and nuance.`,
-        image: "/design-utils/temp/paper-cup.png",
-      },
-      {
-        title: `Iced Espresso`,
-        description: `Our signature Espresso Roast over ice boasts rich, robust flavor. A recipe of perfection at the heart of handcrafted espresso we serve.`,
-        image: "/design-utils/temp/paper-cup.png",
-      },
-      {
-        title: `Lemonade`,
-        description: `Awaken your taste buds with the zing of refreshing lemonade—this tangy, fresh sip puts a little zip in your step.`,
-        image: "/design-utils/temp/lemonade.png",
-      },
-      {
-        title: `Iced Pineapple Matcha`,
-        description: `Our premium matcha green tea shaken with flavors of pineapple and ginger along with coconutmilk and ice for a delicious beverage to uplift your day.`,
-        image: "/design-utils/temp/iced-pineapple-matcha.png",
-      },
-    ],
-  },
-};
 const fakeData_two = {
   restaurantName: "main",
   restaurantMenu: [
     {
-      _id: { $oid: "601ae91d264f601888190622" },
       title: "`double steakhouse`",
       description:
         "`The Double Steakhouse is the only burger for the real gourmets. With no fewer than two pieces of 100% Flame Grilled Beef.`",
       image: "/design-utils/temp/double-steakhouse.png",
       category: "beaf",
       type: "burger",
+      orderedTimes: 8,
     },
     {
-      _id: { $oid: "601ae91d264f601888190623" },
       title: "`double bacon`",
       description:
         "`Bacon, cheese, ketchup and mayonnaise on a flame grilled beef burger. With 2x beef it is also twice as delicious!`",
       image: "/design-utils/temp/double-bacon-burger.png",
       category: "beaf",
       type: "burger",
+      orderedTimes: 10,
     },
     {
-      _id: { $oid: "601ae91d264f601888190624" },
       title: "`plant-base whopper`",
       description:
         "`The WHOPPER® for citizens who love plants more. The plant-based WHOPPER® contains a vegetable hamburger and vegetable mayonnaise.`",
       image: "/design-utils/temp/plant-based-whopper.png",
       category: "beaf",
       type: "burger",
+      orderedTimes: 6,
     },
     {
-      _id: { $oid: "601ae91d264f601888190625" },
       title: "`hamburger`",
       description:
         "`First there was the burger. Then the King came. The Hamburger from BURGER KING® is “the real thing”`",
       image: "/design-utils/temp/hamburger.png",
       category: "beaf",
       type: "burger",
+      orderedTimes: 15,
     },
     {
-      _id: { $oid: "601ae91d264f601888190626" },
       title: "`cheese lover chicken`",
       description:
         "`Is the cheese and chicken combination your favorite? Go for the Cheese Lover Chicken. A freshly toasted and delicious choice.`",
       image: "/design-utils/temp/cheese-lover-chicken.png",
       category: "chicken",
       type: "burger",
+      orderedTimes: 9,
     },
     {
-      _id: { $oid: "601ae91d264f601888190627" },
       title: "`long chicken`",
       description:
         "`Long ago you could enjoy Chicken Burger for a short time. Until we came up with the Long Chicken; a sesame bun with a spicy chicken breast fillet.`",
       image: "/design-utils/temp/long-chicken.png",
       category: "chicken",
       type: "burger",
+      orderedTimes: 1,
     },
     {
-      _id: { $oid: "601ae91d264f601888190628" },
       title: "`crunchy chicken deluxe`",
       description:
         "`We have chicken and we have CHICKEN! The Crunchy Chicken Deluxe certainly belongs to the latter category.`",
       image: "/design-utils/temp/crunchy-chicken-deluxe.png",
       category: "chicken",
       type: "burger",
+      orderedTimes: 1,
     },
     {
-      _id: { $oid: "601ae91d264f601888190629" },
       title: "`crispy chicken`",
       description:
         "`Crunchy on the outside, soft on the inside. The Crispy Chicken has everything you would expect from a chicken burger.`",
       image: "/design-utils/temp/crispy-chicken.png",
       category: "chicken",
       type: "burger",
+      orderedTimes: 1,
     },
     {
-      _id: { $oid: "601ae91d264f60188819062a" },
       title: "`BBQ mixed grill`",
       description:
         "`This barbecue mixed grill pizza has everything you want! Divine BBQ sauce, fresh mozzarella, onion and bell pepper.`",
       image: "/design-utils/temp/bbq-mixed-grill.png",
       category: "other",
       type: "pizza",
+      orderedTimes: 8,
     },
     {
-      _id: { $oid: "601ae91d264f60188819062b" },
       title: "`Vegan Shawarma`",
       description:
         "`The Vegan Shawarma pizza is one especially for you as a vegetarian or vegan. There are no animal products in the pizza.`",
       image: "/design-utils/temp/vegan-shawarma.png",
       category: "other",
       type: "pizza",
+      orderedTimes: 7,
     },
     {
-      _id: { $oid: "601ae91d264f60188819062c" },
       title: "`Hot Chicken`",
       description:
         "`The Hot Chicken pizza is for people who love chicken and spicy food. This delicious pizza is a real favorite of everyone.`",
       image: "/design-utils/temp/hot-chicken.png",
       category: "other",
       type: "pizza",
+      orderedTimes: 12,
     },
     {
-      _id: { $oid: "601ae91d264f60188819062d" },
       title: "`hot thai chicken`",
       description:
         "`Anyone who loves Asian food has ordered Thai food. And those who love Thai, lucky enough to order the Hot Thai Chicken Pizza.`",
       image: "/design-utils/temp/hot-thai-chicken.png",
       category: "other",
       type: "pizza",
+      orderedTimes: 15,
     },
     {
-      _id: { $oid: "601ae91d264f60188819062e" },
       title: "`Brooklyn Style`",
       description:
         "`Real American pizza: the Brooklyn Style pizza is a real American classic. The pizza is one of our toppers for a reason.`",
       image: "/design-utils/temp/brooklyn-style.png",
       category: "other",
       type: "pizza",
+      orderedTimes: 10,
     },
     {
-      _id: { $oid: "601ae91d264f60188819062f" },
       title: "`Pepperoni Perfection`",
       description:
         "`The Pepperoni Perfection is a delicious variation on our successful Double Pepperoni pizza where everything revolves around super tasty pepperoni.`",
       image: "/design-utils/temp/pepperoni-perfection.png",
       category: "other",
       type: "pizza",
+      orderedTimes: 11,
     },
     {
-      _id: { $oid: "601ae91d264f601888190630" },
       title: "`New York`",
       description:
         "`The New York pizza is the perfect lunch or evening meal. The New York pizza is topped with red tomato sauce, ground beef and turkey ham.`",
       image: "/design-utils/temp/new-york.png",
       category: "other",
       type: "pizza",
+      orderedTimes: 1,
     },
     {
-      _id: { $oid: "601ae91d264f601888190631" },
       title: "`Caffee Americano`",
       description:
         "`Espresso shots topped with hot water create a light layer of crema culminating in this wonderfully rich cup with depth and nuance.`",
       image: "/design-utils/temp/paper-cup.png",
       category: "hot",
       type: "drink",
+      orderedTimes: 20,
     },
     {
-      _id: { $oid: "601ae91d264f601888190632" },
       title: "`Espresso`",
       description:
         "`Our smooth signature Espresso Roast with rich flavour and caramelly sweetness is at the very heart of everything we do.`",
       image: "/design-utils/temp/paper-cup.png",
       category: "hot",
       type: "drink",
+      orderedTimes: 12,
     },
     {
-      _id: { $oid: "601ae91d264f601888190633" },
       title: "`Hot Chocolate`",
       description:
         "`Steamed milk and mocha sauce topped with sweetened whipped cream and a chocolate-flavoured drizzle. A timeless classic made to sweeten your spirits.`",
       image: "/design-utils/temp/hot-chocolate.png",
       category: "hot",
       type: "drink",
+      orderedTimes: 15,
     },
     {
-      _id: { $oid: "601ae91d264f601888190634" },
       title: "`Green Tea`",
       description:
         "`Pure green tea is steamed with milk. A perfect hot companion day or night.`",
       image: "/design-utils/temp/green-tea.png",
       category: "hot",
       type: "drink",
+      orderedTimes: 1,
     },
     {
-      _id: { $oid: "601ae91d264f601888190635" },
       title: "`Iced Caffè Americano`",
       description:
         "`Espresso shots topped with cold water produce a light layer of crema, then served over ice. The result: a wonderfully rich cup with depth and nuance.`",
       image: "/design-utils/temp/paper-cup.png",
       category: "cold",
       type: "drink",
+      orderedTimes: 1,
     },
     {
-      _id: { $oid: "601ae91d264f601888190636" },
       title: "`Iced Espresso`",
       description:
         "`Our signature Espresso Roast over ice boasts rich, robust flavor. A recipe of perfection at the heart of handcrafted espresso we serve.`",
       image: "/design-utils/temp/paper-cup.png",
       category: "cold",
       type: "drink",
+      orderedTimes: 1,
     },
     {
-      _id: { $oid: "601ae91d264f601888190637" },
       title: "`Lemonade`",
       description:
         "`Awaken your taste buds with the zing of refreshing lemonade—this tangy, fresh sip puts a little zip in your step.`",
       image: "/design-utils/temp/lemonade.png",
       category: "cold",
       type: "drink",
+      orderedTimes: 18,
     },
     {
-      _id: { $oid: "601ae91d264f601888190638" },
       title: "`Iced Pineapple Matcha`",
       description:
         "`Our premium matcha green tea shaken with flavors of pineapple and ginger along with coconutmilk and ice for a delicious beverage to uplift your day.`",
       image: "/design-utils/temp/iced-pineapple-matcha.png",
       category: "cold",
       type: "drink",
+      orderedTimes: 1,
     },
   ],
 };
@@ -433,12 +249,14 @@ const useMenuData = () => {
           [type]: itemByType,
         };
       });
+
+      return formattedMenu;
     };
 
-    setTimeout(() => {
-      setActiveType("burger");
-      setMenuData(fakeData);
-    }, 0);
+    const formattedMenu = menuFetch();
+
+    setActiveType(Object.keys(formattedMenu)[0]);
+    setMenuData(formattedMenu);
   }, []);
 
   return { menuData, activeType, activeTypeHandler };
