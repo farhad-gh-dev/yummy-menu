630824bd9d4a0b07ffa364169634e8b3129b6626
source-architect <source.architect@gmail.com>
2021-03-27 23:01:57 +0430

    working on desktop mobile model

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -33,62 +33,64 @@ export default function signUp() {
         </Head>
 
         <Alert text={authError.text} type={authError.type} />
-        <AuthFormPanel
-          type="sign up"
-          redirectLink={{
-            description: "already have an account,",
-            buttonTitle: "sign in",
-            to: "/sign-in",
-          }}
-          guestUser={guestUser}
-          formData={formData}
-          submitHandler={signUpHandler}
-        >
-          <div className="input-container">
-            <input
-              type="text"
-              name="email"
-              value={formData.email}
-              placeholder="Email Address"
-              autoComplete="off"
-              onChange={(e) => formHandler(e)}
-              onKeyDown={(e) => e.key === "Enter" && signUpHandler(formData)}
-            />
-          </div>
-          <div className="input-container">
-            <input
-              type="text"
-              name="username"
-              autoComplete="off"
-              value={formData.username}
-              placeholder="Username"
-              onChange={(e) => formHandler(e)}
-              onKeyDown={(e) => e.key === "Enter" && signUpHandler(formData)}
-            />
-          </div>
-          <div className="input-container">
-            <input
-              type={showPassword ? "text" : "password"}
-              name="password"
-              value={formData.password}
-              placeholder="Password"
-              onChange={(e) => formHandler(e)}
-              onKeyDown={(e) => e.key === "Enter" && signUpHandler(formData)}
-            />
-            <div
-              className="input-icon"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              <div className="p-relative">
-                <img
-                  src={`/design-utils/show-password.png`}
-                  alt="show password"
-                />
-                {showPassword && <div className="hide-password"></div>}
+        <div className="section-container h-100">
+          <AuthFormPanel
+            type="sign up"
+            redirectLink={{
+              description: "already have an account,",
+              buttonTitle: "sign in",
+              to: "/sign-in",
+            }}
+            guestUser={guestUser}
+            formData={formData}
+            submitHandler={signUpHandler}
+          >
+            <div className="input-container">
+              <input
+                type="text"
+                name="email"
+                value={formData.email}
+                placeholder="Email Address"
+                autoComplete="off"
+                onChange={(e) => formHandler(e)}
+                onKeyDown={(e) => e.key === "Enter" && signUpHandler(formData)}
+              />
+            </div>
+            <div className="input-container">
+              <input
+                type="text"
+                name="username"
+                autoComplete="off"
+                value={formData.username}
+                placeholder="Username"
+                onChange={(e) => formHandler(e)}
+                onKeyDown={(e) => e.key === "Enter" && signUpHandler(formData)}
+              />
+            </div>
+            <div className="input-container">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={formData.password}
+                placeholder="Password"
+                onChange={(e) => formHandler(e)}
+                onKeyDown={(e) => e.key === "Enter" && signUpHandler(formData)}
+              />
+              <div
+                className="input-icon"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                <div className="p-relative">
+                  <img
+                    src={`/design-utils/show-password.png`}
+                    alt="show password"
+                  />
+                  {showPassword && <div className="hide-password"></div>}
+                </div>
               </div>
             </div>
-          </div>
-        </AuthFormPanel>
+          </AuthFormPanel>
+        </div>
       </div>
     );
 
